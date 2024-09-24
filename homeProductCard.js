51fff625d6777abdde48255d69aa41937fe1e8cb
Mazharrais <mazharmajo72@gmail.com>
2024-09-24 11:49:57 +0500

    add file

---
+++ homeProductCard.js
@@ -0,0 +1,32 @@
+
+
+const productContainer = document.querySelector('#productContainer');
+const productTemplate = document.querySelector('#productTemplate');
+
+
+
+export const showProductContainer = (products) => {
+    if(!products){
+        return false;
+    }
+
+    products.forEach((curProd)=>{
+        const {id,name,category,brand,price,stock,description,image} = curProd;
+
+    const productClone = document.importNode(productTemplate.content,true);
+    productClone.querySelector('.category').textContent = category;
+    productClone.querySelector('.productName').textContent = name;
+    productClone.querySelector('.productImage').src = image;
+    productClone.querySelector('.productImage').alt = name;
+    productClone.querySelector('.productStock').textContent = stock;
+    productClone.querySelector('.productDescription').textContent = description;
+    productClone.querySelector('.productPrice').textContent = `PKR${price}`;
+    productClone.querySelector('.productActualPrice').textContent = `PKR${price * 2}`;
+
+
+
+    productContainer.append(productClone);
+
+
+    })
+}
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import './style.css'
+import './style.css';
+
+
+import products from "./api/product.json";
+
+import {showProductContainer} from "./homeProductCard";
+
+
+showProductContainer(products);
